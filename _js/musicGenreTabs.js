693547ce7857d4ccd281effa6fa7558dd5f0634a
shella688 <obetts28@gmail.com>
2024-10-18 11:35:10 +0100

    song section update

---
+++ _js/musicGenreTabs.js
@@ -0,0 +1,37 @@
+// function openGenre(evt, genre) {
+//     var i, tabcontent, tablinks;
+
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i=0; i< tabcontent.length; i++) {
+//         tabcontent[i].computedStyleMap.display = "none";
+//     }
+
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for(i=0; i<tablinks.length; i++) {
+//         tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
+
+//     document.getElementById(genre).style.display = "block";
+//     evt.currentTarget.className += " active";
+// }
+
+function openGenre(evt, genreName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(genreName).style.display = "block";
+    evt.currentTarget.className += " active";
+  } 
