084af1ae2656ba1cdea02a8374a3cee4994f7106
shella688 <obetts28@gmail.com>
2024-11-10 10:19:11 +0000

    bsides london blog. new blinkies

--- _js/zoneletsScript.js
+++ _js/zoneletsScript.js
@@ -35,6 +35,7 @@ let postsArray = [
 //[ "posts/2020-11-10-Special-Characters-Example.html", encodeURI( 'Spéci@l "Character\'s" Examp|e' ) ]
 //[ "posts/2020-11-10-My-Third-Post-Example.html" ],
 //[ "posts/2020-11-10-My-Second-Post-Example.html" ],
+["posts/2024-11-10-The-time-they-let-me-talk-at-a-respected-cybersecurity-conference.html"],
 ["posts/2023-08-08-Escaping-the-Reverse-Bear-CAPTCHA.html"],
 ["posts/2023-07-31-Is-Instagram-Threads-Falling-Apart-at-the-Seams.html"],
 ["posts/2023-06-07-How-to-make-a-flower-identifying-neural-network-that-won't-kill-you-once-the-robot-uprising-comes.html"],
